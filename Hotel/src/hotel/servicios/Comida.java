912029043c2bc7e2fd152ab096af00cdbdad14d8
Stiven Julio Doval <stjuliod@unal.edu.co>
2023-04-18 21:24:52 -0500

    Update Comida, Limpieza
    Se actualizan las siguientes clases: Comida y Limpieza

--- Hotel/src/hotel/servicios/Comida.java
+++ Hotel/src/hotel/servicios/Comida.java
@@ -1,5 +1,46 @@
 package hotel.servicios;
+import java.util.ArrayList;
 
 public class Comida {
+
+        private String menu;
+        private ArrayList<String> platos;
+      
+        public Comida(String menu) {
+          this.menu = menu;
+          this.platos = new ArrayList<String>();
+        }
+      
+        public void agregarPlato(String plato) {
+          this.platos.add(plato);
+        }
+      
+        public void eliminarPlato(String plato) {
+          this.platos.remove(plato);
+        }
+      
+        public ArrayList<String> getPlatos() {
+          return this.platos;
+        }
+
+        public String getMenu(){
+            return this.menu;
+        }
+        
+        public void setMenu(String menu) {
+            this.menu = menu;
+        }
+
+        public void setPlatos(ArrayList<String> platos) {
+            this.platos = platos;
+        }
+
+        public void imprimirMenu() {
+          System.out.println("=== " + this.menu + " ===");
+          for (String plato : this.platos) {
+            System.out.println("- " + plato);
+          }
+        }
+  
     
 }
--- Hotel/src/hotel/servicios/Limpieza.java
+++ Hotel/src/hotel/servicios/Limpieza.java
@@ -1,5 +1,29 @@
 package hotel.servicios;
 
 public class Limpieza {
+
+    private String nombre;
+    private int costo;
+    
+    public Limpieza(String nombre, int costo) {
+        this.nombre = nombre;
+        this.costo = costo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
     
 }
